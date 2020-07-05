28650e1da2e25470a1be9a51de1822fd8446abc8
Radhika Patel <radz@Radhikas-MacBook-Air.local>
2020-07-05 10:42:44 +0530

    First React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Import React and ReactDOM
+import React from "react";
+import ReactDOM from "react-dom";
+// Create React Component
+const App = () => {
+  const labelText = "Enter Name: ";
+  const buttonText = "Submit";
+  return (
+    <div>
+      <h1>Part 1</h1>
+      <h1>Hello World!</h1>
+      <hr />
+      <h1>Part 2</h1>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button
+        style={{
+          marginLeft: "10px",
+          backgroundColor: "royalblue",
+          color: "white",
+        }}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+// Take React Component and display it on the screen
+ReactDOM.render(<App />, document.getElementById("root"));
